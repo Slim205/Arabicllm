e39c1415abbab179b14dd1adee6fb9e11aa7f362
Slim BARKALLAH <140338365+Slim205@users.noreply.github.com>
2024-07-02 18:28:28 +0200

    Update count_token.py

--- count_token.py
+++ count_token.py
@@ -7,12 +7,14 @@ tokenizer = AutoTokenizer.from_pretrained("meta-llama/Meta-Llama-3-8B-Instruct")
 # Load the dataset
 dataset = load_dataset("CohereForAI/aya_collection_language_split", "standard_arabic")
 #dataset = load_dataset("Muennighoff/xP3x", "arb_Arab",trust_remote_code=True)
+
 print('Data loaded')
 
 def count_tokens(examples):
-    inp_out = [inp + out for inp, out in zip(examples['inputs'], examples['targets'])]
-    tokenized_outputs = tokenizer(inp_out, truncation=True, padding=True)
-    examples['num_tokens'] = [len(input_ids) for input_ids in tokenized_outputs['input_ids']]
+    tokenized_inputs = tokenizer(examples['inputs'])
+    tokenized_outputs = tokenizer(examples['targets'])
+    examples['num_input_tokens'] = [len(input_ids) for input_ids in tokenized_inputs['input_ids']]
+    examples['num_output_tokens'] = [len(input_ids) for input_ids in tokenized_outputs['input_ids']]
     return examples
 
 # Apply the function to the dataset with batched=True
@@ -23,11 +25,25 @@ tokenized_dataset = dataset.map(count_tokens, batched=True)
 splits_to_check = ['train', 'test', 'validation']
 
 # Summarize the token counts
-total_tokens = 0
+total_input_tokens = 0
+total_output_tokens = 0
+split_tokens = {}
+
 for split in splits_to_check:
     if split in tokenized_dataset:
-        num_tokens = sum(tokenized_dataset[split]['num_tokens'])
-        print(f"Number of tokens in {split} set: {num_tokens}")
-        total_tokens += num_tokens
+        num_input_tokens = sum(tokenized_dataset[split]['num_input_tokens'])
+        num_output_tokens = sum(tokenized_dataset[split]['num_output_tokens'])
+        split_tokens[split] = {'input': num_input_tokens, 'output': num_output_tokens}
+        print(f"Number of input tokens in {split} set: {num_input_tokens}")
+        print(f"Number of output tokens in {split} set: {num_output_tokens}")
+        total_input_tokens += num_input_tokens
+        total_output_tokens += num_output_tokens
 
-print('Total number of tokens in the dataset:', total_tokens)
+print('Total number of input tokens in the dataset:', total_input_tokens)
+print('Total number of output tokens in the dataset:', total_output_tokens)
+
+# Print total tokens per split
+for split, tokens in split_tokens.items():
+    print(f"{split.capitalize()} set - Input tokens: {tokens['input']}, Output tokens: {tokens['output']}")
+
+
