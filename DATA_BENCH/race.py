9fe88624719bf3667a83d69f0ac8a215481c9c6c
slim.barkallah@student-cs.fr <barkallasl@ruche02.cluster>
2024-08-14 17:19:56 +0200

    update

--- DATA_BENCH/race.py
+++ DATA_BENCH/race.py
@@ -3,18 +3,19 @@ from vllm import LLM,SamplingParams
 from datasets import load_dataset, DatasetDict,Dataset
 from transformers import AutoTokenizer
 import torch
+
 def give_answer(id,options) :
     index = ord(id)-ord('A')
     return options[index]
 
-def get_question_template(sample) : 
-    prompt = f"""
-Read the following text and answer the question provided:
+def get_instruction(question, text):
+    instruction = f"""
+    {text}
 
-Text: {sample['article']}
-Question: {sample['question']}
-"""
-    return prompt
+    {question}
+    """
+    return instruction
+
 def race(model_name: str, repo_name: str):
 
     dataset = load_dataset("ehovy/race","all")
@@ -23,8 +24,7 @@ def race(model_name: str, repo_name: str):
 
     prompts = []
 
-    def apply_chat_template(sample) : 
-
+    def get_answer(sample) : 
         prompt = f"""
 Please read the text below and provide a more detailed response to the question that follows, without mentioning or referring to the original text:
 
@@ -34,18 +34,28 @@ Answer: {give_answer(sample['answer'],sample['options'])}
 
 Generate your response without including any tags, comments, or references to the provided text.
 """
-        messages = [
-            {"role": "user", "content":prompt},
+        messages = [{"role": "user", "content":prompt}]
+        prompt_with_chat_template= tokenizer.apply_chat_template(messages, add_generation_prompt=True,  tokenize=False)
+        return prompt_with_chat_template
+    def get_question(sample) : 
+        prompt = f"""
+Read the text below and reformulate the question based on its content:
 
-        ]
+Text: {sample['article']}
+Question: {sample['question']}
+
+Please provide your reformulated question without including any tags, comments, or direct references to the text.
+"""
+
+
+        messages = [{"role": "user", "content":prompt}]
         prompt_with_chat_template= tokenizer.apply_chat_template(messages, add_generation_prompt=True,  tokenize=False)
         return prompt_with_chat_template
 
-    llm_questions = []
 
     for example in dataset:        
-        prompts.append(apply_chat_template(example))
-        llm_questions.append(get_question_template(example))
+        prompts.append(get_question(example))
+        prompts.append(get_answer(example))
 
     print(prompts[0])
 
@@ -54,13 +64,19 @@ Generate your response without including any tags, comments, or references to th
 
     sampling_params = SamplingParams(max_tokens=512,temperature=0.8, top_p=0.95)
     outputs = llm.generate(prompts,sampling_params)
-
-    llm_answers=[]
-    for i, item in enumerate(outputs):
-        llm_answers.append(item.outputs[0].text)
     
+    llm_answers=[]
+    llm_questions=[]
+    for i, item in enumerate(outputs):
+
+        # llm_questions.append(item.outputs[0].text)
+        if i % 2 == 0 :
+            llm_questions.append(get_instruction(item.outputs[0].text,dataset['article'][i//2]))
+        else :
+            llm_answers.append(item.outputs[0].text)
+
+    dataset = dataset.add_column("ift_instruction", llm_questions)
     dataset = dataset.add_column("ift_answer", llm_answers)
-    dataset = dataset.add_column("ift_questions", llm_questions)
 
     dataset_dict = DatasetDict({"train": dataset})
 
