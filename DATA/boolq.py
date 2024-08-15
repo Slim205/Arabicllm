55ed218e0350e9a6e614bf98ee3a5656cae1c4f3
slim.barkallah@student-cs.fr <barkallasl@ruche02.cluster>
2024-08-15 17:32:35 +0200

    update boolq.py

--- DATA/boolq.py
+++ DATA/boolq.py
@@ -4,18 +4,25 @@ from datasets import load_dataset, DatasetDict,Dataset
 from transformers import AutoTokenizer
 import torch
 
+def get_instruction(question, text):
+    instruction = f"""
+    « {text} »
+
+    {question}
+    """
+    return instruction
+
+
 def boolq(model_name: str, repo_name: str):
 
     dataset = load_dataset("google/boolq")
     dataset = dataset['train'].select(range(50))
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    prompts = []
 
-    def apply_chat_template(sample) : 
-
+    def get_answer(sample) : 
         prompt = f"""
-Please read the text below and provide a more detailed response to the question that follows, without mentioning or referring to the original text:
+Read the text below and answer the following question with a brief justification (one sentence). Start your response with "{str(sample['answer'])}," : 
 
 Text: {sample['passage']}
 Question: {sample['question']}
@@ -30,15 +37,14 @@ Generate your response without including any tags, comments, or references to th
         prompt_with_chat_template= tokenizer.apply_chat_template(messages, add_generation_prompt=True,  tokenize=False)
         return prompt_with_chat_template
 
+    prompts = []
+    llm_questions = []
     for example in dataset:        
-        prompts.append(apply_chat_template(example))
-
+        prompts.append(get_answer(example))
+        llm_questions.append(get_instruction(example['question'],example['passage']) )
     print(prompts[0])
 
-    if model_name == "google/gemma-2-27b-it" :
-        llm = LLM(model_name, dtype=torch.float16,tensor_parallel_size=2,max_model_len=2048,gpu_memory_utilization=0.8) 
-    else : 
-        llm = LLM(model_name, dtype=torch.float16,max_model_len=2048) 
+    llm = LLM(model_name, dtype=torch.float16,max_model_len=2048) 
 
     sampling_params = SamplingParams(max_tokens=512,temperature=0.8, top_p=0.95)
     outputs = llm.generate(prompts,sampling_params)
@@ -46,8 +52,10 @@ Generate your response without including any tags, comments, or references to th
     llm_answers=[]
     for i, item in enumerate(outputs):
         llm_answers.append(item.outputs[0].text)
-    
+
+    dataset = dataset.add_column("ift_instruction", llm_questions)
     dataset = dataset.add_column("ift_answer", llm_answers)
+
     dataset_dict = DatasetDict({"train": dataset})
 
    # dataset_dict.save_to_disk(output_path)
