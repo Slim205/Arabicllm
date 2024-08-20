ec91be776b11be4cc4d5c279ee69eb3e7427e9d7
slim.barkallah@student-cs.fr <barkallasl@ruche01.cluster>
2024-08-20 23:01:42 +0200

    wikipedia

--- Wikipedia/wiki_data_ar.py
+++ Wikipedia/wiki_data_ar.py
@@ -27,38 +27,22 @@ def get_list_titles(page) :
     #l = l[:pos]
     return l 
 
-def get_question(text,tokenizer) : 
+def get_data(text,tokenizer) : 
     prompt = f"""
-Read the text below and create a question whose answer is the entire text.
+Read the text below and create a question-answer pair based on it. Separate the question and answer with "$".
 
 {text}
 
-Provide only the question, without any tags, comments, or references to the input text.
+Provide only the question and answer, without any tags, comments, or references to the input text.
 """
-
+    
     messages = [{"role": "user", "content":prompt}]
     prompt_with_chat_template= tokenizer.apply_chat_template(messages, add_generation_prompt=True,  tokenize=False)
     return prompt_with_chat_template
-
-#Provide a detailed answer to the following question based on the provided text. 
-
-def get_answer(text,question,tokenizer) : 
-    prompt = f"""
-Answer to the following question based on the provided text. 
-
-Text: {text}
-Question : {question}
-
-Respond directly, avoiding any tags, comments, or references to the input text.
-"""
-    messages = [{"role": "user", "content":prompt}]
-    prompt_with_chat_template= tokenizer.apply_chat_template(messages, add_generation_prompt=True,  tokenize=False)
-    return prompt_with_chat_template
-
 
 def wiki( model_name : str,repo_name : str):
-    #titles = load_list_from_file('wikipedia_links_list.txt')
-   # titles = titles[:50]
+    titles = load_list_from_file('wikipedia_links_list.txt')
+    titles = titles[:50]
     titles = [
     'Algeria', 'Ancient Egypt', 'Caliphate', 'Islamic architecture', 'Islamic art',
     'Astronomy in the medieval Islamic world', 'Arabic calligraphy', 'Arab culture',
@@ -71,32 +55,31 @@ def wiki( model_name : str,repo_name : str):
     'Morocco', 'Oman', 'State of Palestine', 'Qatar', 'Saudi Arabia', 'Somalia', 'Sudan',
     'Syria', 'Tunisia', 'United Arab Emirates', 'Yemen'
 ]
+
     #model_name = "meta-llama/Meta-Llama-3.1-8B-Instruct"
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     print(titles[0])
-    if model_name == "google/gemma-2-27b-it" :
-        llm = LLM(model_name,max_model_len=4096, tensor_parallel_size=2,gpu_memory_utilization=0.8)
-    else :
-        llm = LLM(model_name,max_model_len=4096) 
+    llm = LLM(model_name,max_model_len=4096) 
     list_passages = []
     list_titles =[]
     list_links = []
     prompt = []
-
     for title in titles :
         page = wiki_wiki.page(title)
-        l = get_list_titles(page)
-        list_text = []
-        for title in l : 
-            text_title = page.section_by_title(title).text
-            if len(text_title) > 100 : 
-                list_text.append(text_title)
-      #  list_text.sort(key=lambda x: len(x), reverse=True)
-        for text in list_text: #[:10] :
-            prompt.append(get_question(text,tokenizer))
-            list_passages.append(text)
-            list_links.append(page.fullurl)
-            list_titles.append(page.title)
+        if 'ar' in page.langlinks : 
+            page = page.langlinks['ar']
+            l = get_list_titles(page)
+            list_text = []
+            for title in l : 
+                text_title = page.section_by_title(title).text
+                if len(text_title) > 100 : 
+                    list_text.append(text_title)
+        #  list_text.sort(key=lambda x: len(x), reverse=True)
+            for text in list_text: #[:10] :
+                prompt.append(get_data(text,tokenizer))
+                list_passages.append(text)
+                list_links.append(page.fullurl)
+                list_titles.append(page.title)
     print(prompt[0])
 
     sampling_params = SamplingParams(max_tokens=512,temperature=0.8, top_p=0.95)
@@ -108,19 +91,13 @@ def wiki( model_name : str,repo_name : str):
     llm_links = []
     for i, item in enumerate(outputs):
         output = item.outputs[0].text
-        llm_questions.append(output)
-        llm_passage.append(list_passages[i])
-        llm_titles.append(list_titles[i])
-        llm_links.append(list_links[i])
+        if '$' in output : 
+            llm_questions.append(output.split('$')[0])
+            llm_answers.append(output.split('$')[1])
+            llm_passage.append(list_passages[i])
+            llm_titles.append(list_titles[i])
+            llm_links.append(list_links[i])
 
-    prompts = []
-    for i in range(len(llm_questions)) : 
-        prompts.append(get_answer(llm_passage[i],llm_questions[i],tokenizer))
-
-    outputs = llm.generate(prompts,sampling_params)
-    for i, item in enumerate(outputs):
-        output = item.outputs[0].text
-        llm_answers.append(output)
     data = { "title": llm_titles,"passage" : llm_passage , "question": llm_questions, "answer" : llm_answers, 'link':llm_links}
     dataset = Dataset.from_dict(data)
     dataset_dict = DatasetDict({"train": dataset})
